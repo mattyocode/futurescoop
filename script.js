1b7429d772577cd0e105d3a94e7f039a50a027e4
mattyocode <matthewoliver@live.co.uk>
2021-05-29 21:34:46 +0100

    initial three set up fix

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+// import "./style.css";
+// import * as THREE from "three";
+
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
@@ -23,63 +25,43 @@ let scene = new ScrollMagic.Scene({
   .setPin("section")
   .addTo(controller);
 
-function threeInit() {
-  let threeScene, camera, renderer;
-  threeScene = new THREE.Scene();
-  threeScene.background = new THREE.Color(0xdddddd);
+const canvas = document.querySelector("canvas.webgl");
 
-  camera = new THREE.PerspectiveCamera(
-    40,
-    window.innerWidth / window.innerHeight,
-    1,
-    5000
-  );
-  camera.rotation.y = (45 / 180) * Math.PI;
-  camera.position.x = 800;
-  camera.position.y = 100;
-  camera.position.z = 1000;
+const loader = new THREE.GLTFLoader();
+loader.load(
+  "./assets/scene.gltf",
+  function (gltf) {
+    THREE.Scene.add(gltf.scene);
 
-  controls = new THREE.OrbitControls(camera);
-  controls.addEventListener("change", renderer);
+    gltf.animations;
+    gltf.scene;
+    gltf.scenes; // Array<THREE.Group>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
 
-  hlight = new THREE.AmbientLight(0x404040, 100);
-  threeScene.add(hlight);
+const threeScene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-  directionalLight.position.set(0, 1, 0);
-  directionalLight.castShadow = true;
-  threeScene.add(directionalLight);
-
-  light = new THREE.PointLight(0xc4c4c4, 10);
-  light.position.set(0, 300, 500);
-  threeScene.add(light);
-
-  light2 = new THREE.PointLight(0xc4c4c4, 10);
-  light2.position.set(500, 100, 0);
-  threeScene.add(light2);
-
-  light3 = new THREE.PointLight(0xc4c4c4, 10);
-  light3.position.set(0, 100, -500);
-  threeScene.add(light3);
-
-  light4 = new THREE.PointLight(0xc4c4c4, 10);
-  light4.position.set(-500, 300, 500);
-  threeScene.add(light4);
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  let loader = new THREE.GLTFLoader();
-  loader.load("./assets/scene.gltf", function (gltf) {
-    car = gltf.threeScene.children[0];
-    car.scale.set(0.5, 0.5, 0.5);
-    threeScene.add(gltf.threeScene);
-    animate();
-  });
-}
 function animate() {
-  renderer.render(threeScene, camera);
   requestAnimationFrame(animate);
+  renderer.render(threeScene, camera);
 }
-threeInit();
+animate();
